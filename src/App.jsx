bedee8b3d40c468e6416deb21722f59c98f9366b
John Sison <jlsison777@gmail.com>
2024-03-07 13:15:30 -0500

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
+import "./styles.css"
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title,
+          completed: false
+        }
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
-
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
 }
-
-export default App
